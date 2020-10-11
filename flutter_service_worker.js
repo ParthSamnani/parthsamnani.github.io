1895eea53b413ecbd3ea38c7d4e747b7b2747bbc
Parth Samnani <44186451+ParthSamnani@users.noreply.github.com>
2020-10-12 02:20:42 +0530

    updating files

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,17 +3,50 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "d892fa006791bc47fa0ba489be31fca2",
-"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+  "main.dart.js": "48b7ef7151f191a83c72839cb9aa9a47",
+"assets/FontManifest.json": "bdd5ce6f199dd9c206f745254bec9bfd",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "d80ca32233940ebadc5ae5372ccd67f9",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "a126c025bab9a1b4d8ac5534af76a208",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "831eb40a2d76095849ba4aecd4340f19",
+"assets/packages/flutter_feather_icons/fonts/feather.ttf": "c96dc22ca29a082af83cce866d35cebc",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/NOTICES": "75f844c915b80ed4008257142b307475",
-"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/assets/screenshots/portfolio_design_2.png": "711a86e370566ce01c1903db1aa4e794",
+"assets/assets/screenshots/portfolio_1.gif": "18dd0909f3aba633013d389421334c87",
+"assets/assets/screenshots/portfolio_2.gif": "4196cf51f9316273f9ef7bfefc97d20b",
+"assets/assets/screenshots/portfolio_mobile_1.gif": "6f5725083b4becfb8b77c2af45a7e165",
+"assets/assets/screenshots/portfolio_design_1.png": "7d6b918c406123f221b50e48feea493f",
+"assets/assets/screenshots/portfolio_3.gif": "a485ce18b008accbc91075f7aa27e2e7",
+"assets/assets/screenshots/portfolio_design_3.png": "f24dd0da7633fba80106b2865358a785",
+"assets/assets/fonts/proxima_nova/ProximaNova-Thin.otf": "8f0bc01ce5e5becef482d277cb72b728",
+"assets/assets/fonts/proxima_nova/ProximaNova-Bold.otf": "62d4d7d369292a9bf23762465ec6d704",
+"assets/assets/fonts/proxima_nova/ProximaNova-Regular.otf": "410504d49238e955ba7dc23a7f963021",
+"assets/assets/fonts/circe/Circe-Thin.ttf": "8a6928dabfc92fdf4fa5fa4f53534cab",
+"assets/assets/fonts/circe/Circe-Bold.ttf": "22f16521a7d16c7f8342680cf30b46bd",
+"assets/assets/fonts/circe/Circe-Light.ttf": "9fd3216596f21252abbdbc053769134e",
+"assets/assets/images/onboarding_app.jpg": "3579b51a4590f083dd633d145196bd22",
+"assets/assets/images/foodybite.jpg": "15fa08d9eea454b52afcd280aa044083",
+"assets/assets/images/learn_upp.jpg": "45063d1af568d5f2ad55b3f6eb447233",
+"assets/assets/images/colossal_toons.jpg": "309de4edc7e7540a0717710523e07477",
+"assets/assets/images/finopp.jpg": "e8242ed7f425c6937f87d3a5526e76c5",
+"assets/assets/images/vybz.jpg": "afc36c7d9f5cbaf04ca21d43cee72bce",
+"assets/assets/images/otp_package.jpg": "2a4d40fa2c2968e140a02b767002615e",
+"assets/assets/images/data_science_cert.png": "1364c23105bfbb8c9197f7ad1f9dd245",
+"assets/assets/images/david_legend_2.png": "4fe4eaad48d004e47c0b6f5dd02d1cf2",
+"assets/assets/images/david_legend.png": "b01ae00f2e9b1c9f8acd2294fed944d4",
+"assets/assets/images/associate_android_dev.png": "496dc53d08eec0434eeeae9887955ec8",
+"assets/assets/images/android_basics_cert.png": "3279aee63a95e1b4df4e4999364f8749",
+"assets/assets/images/playstore.png": "18fab95d924ef304111a8efd2620c0a6",
+"assets/assets/images/bequip_logistics.jpg": "d9a33516351b0bb9e7cadc83b8780c66",
+"assets/assets/images/login_catalog.jpg": "8142a609044c0f5b26df2964fe9304c4",
+"assets/assets/images/aerium.jpg": "1cc46a5095ea0c7c487280dcd981d703",
+"assets/NOTICES": "100f42a2c6a4ea446c828f1ace704c5a",
+"assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
+"assets/AssetManifest.json": "8a06ec22380a931520a5236e51bb80a1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "de4d1cd1af55181d6e50b0109928b632",
-"index.html": "5f8aa597cc2b6eecb5a951d7247b4b54",
-"/": "5f8aa597cc2b6eecb5a951d7247b4b54",
+"manifest.json": "4c9322e05f7be520fbdb8319b9308bf8",
+"index.html": "9e996f0c0905603b582efb42ea6e4299",
+"/": "9e996f0c0905603b582efb42ea6e4299",
 "favicon.png": "75738934c6cf2b2024f5222674148f08"
 };
 
@@ -31,8 +64,8 @@ const CORE = [
 self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
-      // Provide a no-cache param to ensure the latest version is downloaded.
-      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'no-cache'})));
+      // Provide a 'reload' param to ensure the latest version is downloaded.
+      return cache.addAll(CORE.map((value) => new Request(value, {'cache': 'reload'})));
     })
   );
 });
@@ -115,7 +148,7 @@ self.addEventListener("fetch", (event) => {
         // Either respond with the cached resource, or perform a fetch and
         // lazily populate the cache. Ensure the resources are not cached
         // by the browser for longer than the service worker expects.
-        var modifiedRequest = new Request(event.request, {'cache': 'no-cache'});
+        var modifiedRequest = new Request(event.request, {'cache': 'reload'});
         return response || fetch(modifiedRequest).then((response) => {
           cache.put(event.request, response.clone());
           return response;
